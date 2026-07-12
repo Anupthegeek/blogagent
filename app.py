8b0f1c86120928ed999c0447c810b859b188ea0f
Anupthegeek <83346051+Anupthegeek@users.noreply.github.com>
2026-07-12 12:48:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Blog", layout="wide")
+
+with open("blog.md", "r", encoding="utf-8") as f:
+    content = f.read()
+
+st.markdown(content)
