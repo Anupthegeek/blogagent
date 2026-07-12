2809814966f92803526b0bf4bca87aa400b5d6ca
Anupthegeek <83346051+Anupthegeek@users.noreply.github.com>
2026-07-12 13:46:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="My Blog", layout="wide")
 
-with open("blog.md", "r", encoding="utf-8") as f:
+with open("blog_post.md", "r", encoding="utf-8") as f:
     content = f.read()
 
 st.markdown(content)
